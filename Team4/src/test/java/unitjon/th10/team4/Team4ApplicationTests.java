66600caf2732151f14ec0cf33a41e7eef0c64a4b
seonghoo1217 <seonghoo1217@naver.com>
2023-09-23 10:43:31 +0900

    chore : gradle initialize setting

---
+++ Team4/src/test/java/unitjon/th10/team4/Team4ApplicationTests.java
@@ -0,0 +1,13 @@
+package unitjon.th10.team4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
